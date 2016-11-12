c6785cde5fe3d1870e61e1f6735f4e9afd3556e6
DatsivStepan <datsivStepan@gmail.com>
2016-11-12 19:15:26 +0200

    add many chnage

--- catalog/controller/module/bestseller.php
+++ catalog/controller/module/bestseller.php
@@ -18,7 +18,6 @@ class ControllerModuleBestSeller extends Controller {
 		$data['products'] = array();
 
 		$results = $this->model_catalog_product->getBestSellerProducts($setting['limit']);
-
 		if ($results) {
 			foreach ($results as $result) {
 				if ($result['image']) {
@@ -50,11 +49,12 @@ class ControllerModuleBestSeller extends Controller {
 				} else {
 					$rating = false;
 				}
-
+                                
 				$data['products'][] = array(
 					'product_id'  => $result['product_id'],
 					'thumb'       => $image,
 					'name'        => $result['name'],
+					'model'        => $result['model'],
 					'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('config_product_description_length')) . '..',
 					'price'       => $price,
 					'special'     => $special,
