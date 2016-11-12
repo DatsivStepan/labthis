fd0c78ac28aef2b3b923ea5fa9c1c4c1e17cd5ac
Kuran777 <vasyl.polianchich@gmail.com>
2016-11-12 14:14:05 +0200

    add sample config

---
+++ admin/config-sample.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://labthis.local/admin/');
+define('HTTP_CATALOG', 'http://labthis.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://labthis.local/admin/');
+define('HTTPS_CATALOG', 'http://labthis.local/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\web_server\OpenServer\domains\labthis.local/admin/');
+define('DIR_SYSTEM', 'D:\web_server\OpenServer\domains\labthis.local/system/');
+define('DIR_LANGUAGE', 'D:\web_server\OpenServer\domains\labthis.local/admin/language/');
+define('DIR_TEMPLATE', 'D:\web_server\OpenServer\domains\labthis.local/admin/view/template/');
+define('DIR_CONFIG', 'D:\web_server\OpenServer\domains\labthis.local/system/config/');
+define('DIR_IMAGE', 'D:\web_server\OpenServer\domains\labthis.local/image/');
+define('DIR_CACHE', 'D:\web_server\OpenServer\domains\labthis.local/system/cache/');
+define('DIR_DOWNLOAD', 'D:\web_server\OpenServer\domains\labthis.local/system/download/');
+define('DIR_UPLOAD', 'D:\web_server\OpenServer\domains\labthis.local/system/upload/');
+define('DIR_LOGS', 'D:\web_server\OpenServer\domains\labthis.local/system/logs/');
+define('DIR_MODIFICATION', 'D:\web_server\OpenServer\domains\labthis.local/system/modification/');
+define('DIR_CATALOG', 'D:\web_server\OpenServer\domains\labthis.local/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '54.218.38.7');
+define('DB_USERNAME', 'gv_user');
+define('DB_PASSWORD', 'JzuNbRa5MF4Uiit6');
+define('DB_DATABASE', 'labthis');
+define('DB_PREFIX', 'oc_');
---
+++ config-sample.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://labthis.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://labthis.local/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\web_server\OpenServer\domains\labthis.local/catalog/');
+define('DIR_SYSTEM', 'D:\web_server\OpenServer\domains\labthis.local/system/');
+define('DIR_LANGUAGE', 'D:\web_server\OpenServer\domains\labthis.local/catalog/language/');
+define('DIR_TEMPLATE', 'D:\web_server\OpenServer\domains\labthis.local/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\web_server\OpenServer\domains\labthis.local/system/config/');
+define('DIR_IMAGE', 'D:\web_server\OpenServer\domains\labthis.local/image/');
+define('DIR_CACHE', 'D:\web_server\OpenServer\domains\labthis.local/system/cache/');
+define('DIR_DOWNLOAD', 'D:\web_server\OpenServer\domains\labthis.local/system/download/');
+define('DIR_UPLOAD', 'D:\web_server\OpenServer\domains\labthis.local/system/upload/');
+define('DIR_MODIFICATION', 'D:\web_server\OpenServer\domains\labthis.local/system/modification/');
+define('DIR_LOGS', 'D:\web_server\OpenServer\domains\labthis.local/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '54.218.38.7');
+define('DB_USERNAME', 'gv_user');
+define('DB_PASSWORD', 'JzuNbRa5MF4Uiit6');
+define('DB_DATABASE', 'labthis');
+define('DB_PREFIX', 'oc_');
